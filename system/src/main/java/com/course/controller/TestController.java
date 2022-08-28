f56a0e51de2ac01bad96895104c66a331ee7f07f
ZeroClian <zeroycl@163.com>
2022-08-28 22:51:20 +0800

    course控台

--- system/src/main/java/com/course/controller/TestController.java
+++ system/src/main/java/com/course/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.course.controller;
 
+import com.course.domain.Test;
+import com.course.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author Justin
@@ -9,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Resource
+    private TestService testService;
+
     @GetMapping("/test")
-    public String test() {
-        return "success";
+    public List<Test> test() {
+        return testService.list();
     }
 }
