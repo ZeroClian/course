befa02310a191994075f7d948d04379479a6a5ce
ZeroClian <zeroycl@163.com>
2022-10-16 19:54:52 +0800

    前端增加校验参数

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -122,34 +122,27 @@ export default {
     },
     save() {
       let _this = this
+      //校验
+      if (!Validator.require(_this.chapter.name, "名称") || !Validator.require(_this.chapter.courseId, "课程ID")) {
+        return
+      }
       _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save', _this.chapter).then((respond) => {
         if (respond.data.success) {
           $("#form-modal").modal("hide");
           _this.list(1);
-          toast.success("保存成功!");
+          Toast.success("保存成功!");
         }
       })
     },
     del(id) {
       let _this = this;
-
-      Swal.fire({
-        title: '确认删除?',
-        text: "删除后不可恢复!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: '确认!'
-      }).then((result) => {
-        if (result.value) {
-          _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((respond) => {
-            if (respond.data.success) {
-              _this.list(1);
-              toast.success("删除成功!")
-            }
-          })
-        }
+      Confirm.show("确认删除大章,删除后不可恢复?", function () {
+        _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((respond) => {
+          if (respond.data.success) {
+            _this.list(1);
+            Toast.success("删除成功!")
+          }
+        })
       })
     }
   }
